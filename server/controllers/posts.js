ad6cf57d69063e621a206110bf086f45f411c974
AniAtWork <anirudhkp.work@gmail.com>
2022-09-29 13:43:45 +0530

    typo

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,7 +13,7 @@ export const getPosts = async(req, res) => {
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    const newPost = new postMessage(post);
+    const newPost = new PostMessage(post);
 
     try {
         await newPost.save();
