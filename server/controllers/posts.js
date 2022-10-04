ffc247764891485057b0f34547e66841c2954818
AniAtWork <anirudhkp.work@gmail.com>
2022-10-04 11:47:04 +0530

    to update posts server side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 //all handlers for routes are kept here
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async(req, res) => {
@@ -22,4 +23,15 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ERROR: error.message});
     }
+}
+
+export const updatePost = async (req,res) => {
+    const { id: _id }= req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+
+    res.json(updatedPost);
 }
